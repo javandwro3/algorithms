407158dd7b2a21405fb00f294b6f432029645999
Jakub Wrąbel <wrabel.jakub@gmail.com>
2017-04-18 20:45:21 +0200

    basic notes

--- src/main/java/pl/jwrabel/trainings/javandwro3/algorithms/simple/Primary.java
+++ src/main/java/pl/jwrabel/trainings/javandwro3/algorithms/simple/Primary.java
@@ -5,11 +5,17 @@ package pl.jwrabel.trainings.javandwro3.algorithms.simple;
  */
 public class Primary {
     public static void main(String[] args) {
-        boolean isPrimary = isPrimary(21);
-        System.out.println("21 is primary: " + isPrimary);
+        int x = 21;
+        boolean isPrimary = isPrimary(x);
+        System.out.println(x + " is primary: " + isPrimary);
     }
 
-    public static boolean isPrimary(int x){
+    public static boolean isPrimary(int x) {
+        //
+        //
+        //
+        //
+
         return true;
     }
 }
