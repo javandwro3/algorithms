8f132d2be2aa95a9dc37a95cbee1429a6dcecd51
Jakub Wrąbel <wrabel.jakub@gmail.com>
2017-04-18 20:58:34 +0200

    basic notes

--- src/main/java/pl/jwrabel/trainings/javandwro3/algorithms/simple/Primary.java
+++ src/main/java/pl/jwrabel/trainings/javandwro3/algorithms/simple/Primary.java
@@ -11,11 +11,11 @@ public class Primary {
     }
 
     public static boolean isPrimary(int x) {
-        //
-        //
-        //
-        //
-
+        for (int i = 2; i < x; i++) {
+            if(x % i == 0){
+                return false;
+            }
+        }
         return true;
     }
 }
