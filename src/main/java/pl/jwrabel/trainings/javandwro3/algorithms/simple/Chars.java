c61d606d3ad30d13e6655038ee43ecb3e7b0d82b
Jakub Wrąbel <wrabel.jakub@gmail.com>
2017-04-18 20:36:38 +0200

    basic notes

--- src/main/java/pl/jwrabel/trainings/javandwro3/algorithms/simple/Chars.java
+++ src/main/java/pl/jwrabel/trainings/javandwro3/algorithms/simple/Chars.java
@@ -7,8 +7,28 @@ public class Chars {
     public static void main(String[] args) {
         char c = '6';
         char c2 = 68; // == 'D'
-        System.out.println((char)97); // ascii 98 = 'a'
+        System.out.println((char)97); // ASCII(98) = 'a'
 
         // === ZADANIE === Wypisać litery a-z
+        System.out.println("--- WYPISANIE LITER a-z");
+        // Sposób 1
+        for (int i = 97; i <= 122; i++) {
+            System.out.println((char)i);
+
+            char letter = (char)i;
+            System.out.println(letter);
+        }
+
+        // Sposób 2
+        char[] alphabet = new char[26];
+        for (int i = 0; i < alphabet.length; i++) {
+            alphabet[i] = (char)(i + 97);
+        }
+
+        for (int i = 0; i < alphabet.length; i++) {
+            System.out.println(alphabet[i]);
+        }
+
+
     }
 }
---
+++ src/main/java/pl/jwrabel/trainings/javandwro3/algorithms/simple/Primary.java
@@ -0,0 +1,15 @@
+package pl.jwrabel.trainings.javandwro3.algorithms.simple;
+
+/**
+ * Created by jakubwrabel on 18.04.2017.
+ */
+public class Primary {
+    public static void main(String[] args) {
+        boolean isPrimary = isPrimary(21);
+        System.out.println("21 is primary: " + isPrimary);
+    }
+
+    public static boolean isPrimary(int x){
+        return true;
+    }
+}
