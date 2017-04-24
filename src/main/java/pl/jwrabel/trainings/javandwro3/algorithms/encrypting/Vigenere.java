18d60c13c8652876cb2d3908ba431866e87d90a4
Jakub Wrąbel <wrabel.jakub@gmail.com>
2017-04-24 18:36:09 +0200

    update

--- src/main/java/pl/jwrabel/trainings/javandwro3/algorithms/encrypting/Vigenere.java
+++ src/main/java/pl/jwrabel/trainings/javandwro3/algorithms/encrypting/Vigenere.java
@@ -1,5 +1,7 @@
 package pl.jwrabel.trainings.javandwro3.algorithms.encrypting;
 
+import pl.jwrabel.trainings.javandwro3.algorithms.simple.MultArrays;
+
 import java.awt.*;
 
 /**
@@ -37,12 +39,59 @@ public class Vigenere {
         char c = exampleText.charAt(2); // 'C'
     }
 
-    public static String encrypt(String text, String  password) {
-        char[] letters = text.toCharArray();
+    public static String encrypt(String text, String password) {
+        char[] letters = text.toLowerCase().toCharArray();
 
 
+        // GENEROWANIE TABLICY VIGENERE'A
+        char[][] vigenereArray = new char[26][26];
+
+        for (int y = 0; y < vigenereArray[0].length; y++) {
+            for (int x = 0; x < vigenereArray.length; x++) {
+//                vigenereArray[x][y] = (char) (97 + x + y);
+//
+//                if (vigenereArray[x][y] > 97 + 25) {
+//                    vigenereArray[x][y] -= 26;
+//                }
+
+                // LUB
+                vigenereArray[x][y] = (char) (97 + (x + y) % 26);
+            }
+        }
+        printArray(vigenereArray);
+
+        // GENEROWANIE HASŁA O ODPOWIEDNIEJ DŁUGOSCI
+        char[] passwordShort = password.toLowerCase().toCharArray();
+        char[] passwordLong = new char[letters.length];
+
+        for (int i = 0; i < passwordLong.length; i++) {
+            passwordLong[i] = passwordShort[i % passwordShort.length];
+            System.out.println(passwordLong[i]);
+        }
+
+        // SZYFROWANIE
+        for (int i = 0; i < letters.length; i++) {
+            char charFromText = letters[i];
+            char charFromPassword = passwordLong[i];
+            System.out.println("Para " + charFromText + " : " + charFromPassword);
+
+            if (charFromText != ' ') {
+                int indexX = charFromText - 97;
+                int indexY = charFromPassword - 97;
+                letters[i] = vigenereArray[indexX][indexY];
+            }
+        }
 
 
         return new String(letters);
+    }
+
+    public static void printArray(char[][] array) {
+        for (int y = 0; y < array[0].length; y++) {
+            for (int x = 0; x < array.length; x++) {
+                System.out.print(array[x][y] + " ");
+            }
+            System.out.println();
+        }
     }
 }
